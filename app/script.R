165e273d90bcd1e778d406c9157207e5797a49ea
Sparsh Tandon <sparsh.tandon@juspay.in>
2021-10-09 14:29:11 +0530

    deleted script

--- app/script.R
+++
@@ -1,8 +0,0 @@
-library(jsonlite)
-url <- 'https://codeforces.com/api/user.rating?handle=ExitCode'
-document <- fromJSON(file=url, method='C')
-yelp <- stream_in(file(url))
-str(yelp)?
-flat_yelp <- flatten(yelp)
-str(flat_yelp)
-yelp_tbl <- as.data.frame(flat_yelp)
