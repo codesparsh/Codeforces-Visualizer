16e42934599a3e89067020026a8281c9fd4597dd
Sparsh Tandon <sparsh.tandon@juspay.in>
2021-10-09 14:16:16 +0530

    modularized

--- home/observers.R
+++ home/observers.R
@@ -12,28 +12,28 @@ observeEvent(input$handle_search,{
     rating_api_data <- readRatingData(handle)
 
     check = tryCatch({
-    if(api_data$status != "FAILED"){
-        DATA$handle <- handle
-        DATA$api_data <- api_data
-        DATA$rating_api_data <- rating_api_data
-        show_modal_spinner(
-            spin = "atom",
-            color = "#112446",
-            text = "Calculating your data",
-        ) 
-        delay(2000, remove_modal_spinner())
-        change_page("analytics")
-        
-    }else{
-        
-        shinyalert("Oops!", "Handle not found", type = "error")
-        updateTextInput(session ,"handle_input",value =  "")
-        
-    }
-}, error = function(e){
-   
-    print(e)
-})
+        if(api_data$status != "FAILED"){
+            DATA$handle <- handle
+            DATA$api_data <- api_data
+            DATA$rating_api_data <- rating_api_data
+            show_modal_spinner(
+                spin = "atom",
+                color = "#112446",
+                text = "Calculating your data",
+            ) 
+            delay(2000, remove_modal_spinner())
+            change_page("analytics")
+            
+        }else{
+            
+            shinyalert("Oops!", "Handle not found", type = "error")
+            updateTextInput(session ,"handle_input",value =  "")
+            
+        }
+    }, error = function(e){
+    
+        print(e)
+    })
 
     
 })
